0a89c36b17fe16ffeace6daa76b71aa0b3946609
plamkutryanska <plamietsy@gmail.com>
2023-08-25 05:30:02 -0500

    Refactor to reduce repeat code

--- assets/components/NearEarthObjectDetails/NearEarthObjectDetails.js
+++ assets/components/NearEarthObjectDetails/NearEarthObjectDetails.js
@@ -1,72 +1,92 @@
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
 
-const NearEarthObjectDetails = ({objectDetails}) => {
+const DetailItem = ({ title, value }) => (
+  <View style={styles.objectDetailsWrapper}>
+    <Text style={styles.objectDetailsTitle}>{title}</Text>
+    <Text>{value}</Text>
+  </View>
+);
+
+const NearEarthObjectDetails = ({ objectDetails }) => {
+  const {
+    name,
+    approximateDiameterInFeet: { minDiameter, maxDiameter } = {},
+    relativeVelocityInMilesPerHour,
+    missDistanceInMiles,
+    potentiallyHazardousAsteroid,
+  } = objectDetails;
+
+  const detailsData = [
+    {
+      title: "Approximate diameter in feet:",
+      value: `Min: ${minDiameter} Max: ${maxDiameter}`,
+    },
+    {
+      title: "Relative velocity in miles per hour:",
+      value: relativeVelocityInMilesPerHour,
+    },
+    { title: "Miss distance in miles:", value: missDistanceInMiles },
+    { title: "Potentially hazardous:", value: potentiallyHazardousAsteroid },
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.sectionHeader}>
-        <Text style={styles.objectName}>{objectDetails.name}</Text>
+        <Text style={styles.objectName}>{name}</Text>
       </View>
       <View style={styles.objectDetailsContainer}>
-        <View style={styles.objectDetailsWrapper}>
-          <Text style={styles.objectDetailsTitle}>Approximate diameter in feet: </Text>
-          <Text>Min: {objectDetails.approximateDiameterInFeet.minDiameter}</Text>
-          <Text>Max: {objectDetails.approximateDiameterInFeet.maxDiameter}</Text>
-        </View>
-        <View style={styles.objectDetailsWrapper}>
-          <Text style={styles.objectDetailsTitle}>Relative velocity in miles per hour: </Text>
-          <Text>{objectDetails.relativeVelocityInMilesPerHour}</Text>
-        </View>
-        <View style={styles.objectDetailsWrapper}>
-          <Text style={styles.objectDetailsTitle}>Miss distance in miles: </Text>
-          <Text>{objectDetails.missDistanceInMiles}</Text>
-        </View>
-        <View style={styles.objectDetailsWrapper}>
-          <Text style={styles.objectDetailsTitle}>Potentially hazardous:</Text>
-          <Text>{objectDetails.potentiallyHazardousAsteroid}</Text>
-        </View>
+        {detailsData.map((details, index) => {
+          return (
+            <DetailItem
+              key={index}
+              title={details.title}
+              value={details.value}
+            />
+          );
+        })}
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default NearEarthObjectDetails
+export default NearEarthObjectDetails;
 
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: 'white',
+    borderColor: "white",
     margin: 20,
-    
   },
   sectionHeader: {
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
     height: 32,
     borderTopLeftRadius: 9,
-    borderTopRightRadius:9
+    borderTopRightRadius: 9,
   },
   objectName: {
-    color: 'white',
-    textTransform: 'uppercase'
+    color: "white",
+    textTransform: "uppercase",
   },
   objectDetailsContainer: {
     minHeight: 200,
     padding: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
     borderBottomRightRadius: 10,
-    borderBottomLeftRadius: 10
+    borderBottomLeftRadius: 10,
   },
   objectDetailsWrapper: {
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexDirection: "column",
+    alignItems: "center",
     marginBottom: 12,
   },
   objectDetailsTitle: {
-    fontWeight: 'bold',
-    fontSize: 14
-  }
+    fontWeight: "bold",
+    fontSize: 14,
+  },
 });
--- assets/components/NearEarthObjects/NearEarthObjects.js
+++ assets/components/NearEarthObjects/NearEarthObjects.js
@@ -1,20 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import DatePicker from "../DatePicker/DatePicker"
-import { getFormattedDate } from '../../utils/getFormattedDate';
-import {getNearEarthObjectDetails} from '../../utils/getNearEarthObjectDetails'
-import EmptyState from '../EmptyState/EmptyState';
-import NearEarthObjectsContainer from './NearEarthObjectsContainer';
-import Loader from '../Loader/Loader';
-import { UseNasaNearEarthObjects } from '../../hooks/useNasaNearEarthObjects';
+import React, { useEffect, useState, useCallback } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import DatePicker from "../DatePicker/DatePicker";
+import EmptyState from "../EmptyState/EmptyState";
+import NearEarthObjectsContainer from "./NearEarthObjectsContainer";
+import Loader from "../Loader/Loader";
 
-const queryClient =  new QueryClient()
+import { getFormattedDate } from "../../utils/getFormattedDate";
+import { getNearEarthObjectDetails } from "../../utils/getNearEarthObjectDetails";
+import { UseNasaNearEarthObjects } from "../../hooks/useNasaNearEarthObjects";
+
+const queryClient = new QueryClient();
 
 const NearEarthObjects = () => {
   const [date, setDate] = useState(new Date());
-  const [formattedDate, setFormattedDate] = useState(getFormattedDate(date))
-  const [showPicker, setShowDatePicker] = useState(false)
+  const [formattedDate, setFormattedDate] = useState(getFormattedDate(date));
+  const [showPicker, setShowDatePicker] = useState(false);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
@@ -23,31 +24,32 @@ const NearEarthObjects = () => {
   };
 
   const showDatePicker = () => {
-    setShowDatePicker(true)
-  }
+    setShowDatePicker(true);
+  };
 
   useEffect(() => {
-    setFormattedDate(getFormattedDate(date))
-  }, [date])
+    setFormattedDate(getFormattedDate(date));
+  }, [date]);
 
-  const {data, isLoading} = UseNasaNearEarthObjects(formattedDate)
+  const { data, isLoading } = UseNasaNearEarthObjects(formattedDate);
 
-  const apiResponse = getNearEarthObjectDetails({data, formattedDate})
+  const apiResponse = getNearEarthObjectDetails({ data, formattedDate });
 
   return (
     <QueryClientProvider client={queryClient}>
-        <DatePicker 
-          date={date}
-          changeSelectedDate={onChange}
-          showPicker={showPicker}
-          showDatePicker={showDatePicker}
-        />
-        {isLoading && <Loader/>}
-        {apiResponse && !isLoading &&
-          <NearEarthObjectsContainer apiResponse={apiResponse}/>}
-        {!apiResponse && !isLoading && <EmptyState/>}
-      </QueryClientProvider>
-  )
-}
+      <DatePicker
+        date={date}
+        changeSelectedDate={onChange}
+        showPicker={showPicker}
+        showDatePicker={showDatePicker}
+      />
+      {isLoading && <Loader />}
+      {apiResponse && !isLoading && (
+        <NearEarthObjectsContainer apiResponse={apiResponse} />
+      )}
+      {!apiResponse && !isLoading && <EmptyState />}
+    </QueryClientProvider>
+  );
+};
 
-export default NearEarthObjects
+export default NearEarthObjects;
