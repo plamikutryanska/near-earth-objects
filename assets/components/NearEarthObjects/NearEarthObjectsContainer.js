a18309985689d67919f80f39ec6ad21c2950a433
plamkutryanska <plamietsy@gmail.com>
2023-08-25 02:36:43 -0500

    Delete old images & commented out code

--- assets/components/NearEarthObjects/NearEarthObjectsContainer.js
+++ assets/components/NearEarthObjects/NearEarthObjectsContainer.js
@@ -1,13 +1,10 @@
-import galaxyImageV2 from '../../../assets/galaxy.jpg'
-import { ScrollView, StyleSheet, View, ImageBackground, Text, ActivityIndicator  } from 'react-native';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
 import NearEarthObjectDetails from "../NearEarthObjectDetails/NearEarthObjectDetails"
 
 
 const NearEarthObjectsContainer = ({apiResponse}) => {
   return (
-    <>
-    {/* <ImageBackground source={galaxyImageV2} alt={'space image'} style={styles.image} resizeMode="cover"> */}
-        <ScrollView style={{backgroundColor: 'black'}}>
+      <ScrollView style={{backgroundColor: 'black'}}>
         <Text style={styles.totalCount}>Total count: {apiResponse?.length}</Text>
         {apiResponse?.map((object, index) => {
           return (
@@ -17,8 +14,6 @@ const NearEarthObjectsContainer = ({apiResponse}) => {
           )
           })}
       </ScrollView>
-    {/* </ImageBackground>  */}
-    </>
   )
 }
 
@@ -27,11 +22,6 @@ export default NearEarthObjectsContainer
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-
   },
   totalCount: {
     color: 'white',
